84b46eae2b988f3c621dd734a732e9f2f648abd5
Tyler Weifenbach <rweifenbach24@gmail.com>
2024-08-14 21:58:14 -0500

    Initialization

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>My Simple React Home Page</h1>
+      </header>
+      <main>
+        <p>THIS IS A TEST. WOO WOO.</p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
