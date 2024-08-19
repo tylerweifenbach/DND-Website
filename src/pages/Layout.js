a88c62386f3f25c56c38d3d8f80320d454f0f1a8
Tyler Weifenbach <rweifenbach24@gmail.com>
2024-08-18 21:25:07 -0500

    Set up a rudimentary React Router setup for navigating pages.

---
+++ src/pages/Layout.js
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom";
+
+//this ensures the navigational layout is present on every page at the top.
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
