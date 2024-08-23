1e14d9873f61a83a9b70e8e727f8da849c0a8d86
Tyler Weifenbach <rweifenbach24@gmail.com>
2024-08-22 22:05:38 -0500

    Implemented styling to the navbar.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,18 +1,18 @@
 import { Outlet, Link } from "react-router-dom";
+import '../Layout.css'; // Import the CSS file for styling
 
-//this ensures the navigational layout is present on every page at the top.
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="nav-item">
             <Link to="/blogs">Blogs</Link>
           </li>
-          <li>
+          <li className="nav-item">
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
