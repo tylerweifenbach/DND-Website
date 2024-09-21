724afce631e2c960af02c19b10c448ca17d04c08
Tyler Weifenbach <rweifenbach24@gmail.com>
2024-09-21 00:48:36 -0500

    Began work on spell database. Set up example/template. Tentatively organizing by spell level.

---
+++ src/spells-level1.js
@@ -0,0 +1,26 @@
+//don't forget imports and exports
+
+const spellsLevel1 = [
+  {
+    name: "Fireball",
+    level: 3,
+    description: "A bright streak flashes from your pointing finger to a point you choose...",
+    effect: "Deals 8d6 fire damage in a 20-foot radius",
+    school: "Evocation",
+    castingTime: "1 action",
+    range: "150 feet",
+    components: ["V", "S", "M (a tiny ball of bat guano and sulfur)"],
+    duration: "Instantaneous"
+  },
+  {
+    name: "Mage Armor",
+    level: 1,
+    description: "You touch a willing creature who isn't wearing armor...",
+    effect: "Grants AC 13 + Dexterity modifier for 8 hours",
+    school: "Abjuration",
+    castingTime: "1 action",
+    range: "Touch",
+    components: ["V", "S", "M (a piece of cured leather)"],
+    duration: "8 hours"
+  }
+];
